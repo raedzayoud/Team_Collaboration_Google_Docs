8ad1ce18862467f81f7c76be572b5713d026a32f
Raed <raed.zayoud.23@gmail.com>
2025-02-19 15:27:18 +0100

    Let Start Our PFA

---
+++ android/app/src/main/kotlin/com/example/team_collaboration_google_docs/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.team_collaboration_google_docs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/feature/presentation/view/login_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:team_collaboration_google_docs/feature/presentation/view/widgets/login_view_body.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LoginViewBody(),
+    );
+  }
+}
